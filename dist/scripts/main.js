a9dfead0505a8d49e0448373840d7040cc8a10ad
danmor383 <dm817828@gmail.com>
2025-01-30 19:47:10 +0700

    pp

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -4,16 +4,19 @@ document.getElementById('pp-button').addEventListener('click', () => {
     }
     else {
         eventPP.style.display = 'flex';
+        document.body.style.overflow = 'hidden';
     }
 })
 
 eventPP.addEventListener('click', (event) => {
     if (event.target === eventPP) {
       eventPP.style.display = 'none';
+      document.body.style.overflow = 'auto';
     }
   })
 
 
 document.getElementById('x-btn__icon-close').addEventListener('click', () => {
     eventPP.style.display = 'none';
+    document.body.style.overflow = 'auto';
 });
